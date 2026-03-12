6ba201d43abab2a1f3424296d17fa5057415d3c8
Andrej Praizovic <pa220300d@student.etf.bg.ac.rs>
2026-03-12 11:54:30 +0100

    Initail Project with DB connection
    Change-Id: I3a1220448a3da74889f60389361c4dbdfe86aeef

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Role(models.Model):
+    idrol = models.AutoField(db_column='IdRol', primary_key=True)  # Field name made lowercase.
+    opis = models.CharField(db_column='Opis', max_length=20)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'role'
+
+class Korisnik(models.Model):
+    idkor = models.AutoField(db_column='IdKor', primary_key=True)  # Field name made lowercase.
+    idrol = models.ForeignKey(Role, models.DO_NOTHING, db_column='IdRol')  # Field name made lowercase.
+    kor_ime = models.CharField(db_column='Kor_Ime', max_length=20)  # Field name made lowercase.
+    lozinka = models.CharField(db_column='Lozinka', max_length=20)  # Field name made lowercase.
+    email = models.CharField(db_column='Email', max_length=18)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'korisnik'
+
+class Prijateljstvo(models.Model):
+    idkor1 = models.ForeignKey(Korisnik, models.DO_NOTHING, db_column='IdKor1', primary_key=True)  # Field name made lowercase. The composite primary key (IdKor1, IdKor2) found, that is not supported. The first column is selected.
+    idkor2 = models.ForeignKey(Korisnik, models.DO_NOTHING, db_column='IdKor2', related_name='prijateljstvo_idkor2_set')  # Field name made lowercase.
+    status = models.CharField(db_column='Status', max_length=20)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'prijateljstvo'
+        unique_together = (('idkor1', 'idkor2'),)
---
+++ materials/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+# Create your models here.
+
+class Kategorija(models.Model):
+    idkat = models.AutoField(db_column='IdKat', primary_key=True)  # Field name made lowercase.
+    naziv = models.CharField(db_column='Naziv', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    tip = models.CharField(db_column='Tip', max_length=20, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'kategorija'
+
+
+class KategorijaNad(models.Model):
+    idkatnad = models.ForeignKey(Kategorija, models.DO_NOTHING, db_column='IdKatNad', primary_key=True)  # Field name made lowercase. The composite primary key (IdKatNad, IdKatPod) found, that is not supported. The first column is selected.
+    idkatpod = models.ForeignKey(Kategorija, models.DO_NOTHING, db_column='IdKatPod', related_name='kategorijanad_idkatpod_set')  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'kategorija_nad'
+        unique_together = (('idkatnad', 'idkatpod'),)
+
+class Skripta(models.Model):
+    idskr = models.AutoField(db_column='IdSkr', primary_key=True)  # Field name made lowercase.
+    idkor = models.ForeignKey('accounts.Korisnik', models.DO_NOTHING, db_column='IdKor', blank=True, null=True)  # Field name made lowercase.
+    idkat = models.ForeignKey(Kategorija, models.DO_NOTHING, db_column='IdKat')  # Field name made lowercase.
+    naziv = models.CharField(db_column='Naziv', max_length=20)  # Field name made lowercase.
+    opis = models.CharField(db_column='Opis', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    fajl = models.TextField(db_column='Fajl')  # Field name made lowercase.
+    odobrena = models.IntegerField(db_column='Odobrena')  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'skripta'
+
+
+class Komentar(models.Model):
+    id = models.AutoField(primary_key=True)
+    idkor = models.ForeignKey('accounts.Korisnik', models.DO_NOTHING, db_column='IdKor')  # Field name made lowercase. The composite primary key (IdKor, IdSkr) found, that is not supported. The first column is selected.
+    idskr = models.ForeignKey(Skripta, models.DO_NOTHING, db_column='IdSkr')  # Field name made lowercase.
+    tekst = models.CharField(db_column='Tekst', max_length=512, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'komentar'
+        unique_together = (('idkor', 'idskr'),)
+
+class Ocena(models.Model):
+    id = models.AutoField(primary_key=True)
+    idkor = models.ForeignKey('accounts.Korisnik', models.DO_NOTHING, db_column='IdKor')  # Field name made lowercase. The composite primary key (IdKor, IdSkr) found, that is not supported. The first column is selected.
+    idskr = models.ForeignKey(Skripta, models.DO_NOTHING, db_column='IdSkr')  # Field name made lowercase.
+    ocena = models.IntegerField(db_column='Ocena', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'ocena'
+        unique_together = (('idkor', 'idskr'),)
+
+class Sacuvano(models.Model):
+    id = models.AutoField(primary_key=True)
+    idkor = models.ForeignKey('accounts.Korisnik'    , models.DO_NOTHING, db_column='IdKor')  # Field name made lowercase. The composite primary key (IdKor, IdSkr) found, that is not supported. The first column is selected.
+    idskr = models.ForeignKey(Skripta, models.DO_NOTHING, db_column='IdSkr')  # Field name made lowercase.
+    kolekcija = models.CharField(db_column='Kolekcija', max_length=18, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'sacuvano'
+        unique_together = (('idkor', 'idskr'),)
